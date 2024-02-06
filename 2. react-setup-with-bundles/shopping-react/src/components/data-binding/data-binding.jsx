08051b99f976e33db0f8f0bb66525dc5c7156b5c
Shutirtha Roy <shutirtha.roy1@gmail.com>
2024-02-06 17:25:39 +1100

    react mount implemented

--- 2. react-setup-with-bundles/shopping-react/src/components/data-binding/data-binding.jsx
+++ 2. react-setup-with-bundles/shopping-react/src/components/data-binding/data-binding.jsx
@@ -1,10 +1,54 @@
+import { useState, useEffect } from "react";
+
 export function DataBinding() {
-    var Stock = true;
+
+    const [product, setProduct] = useState({title: '', rating:{rate: 0, ratings: 0, reviews: 0}, price:0, offers: [], image: ''});
+
+    function LoadProduct() {
+        var http = new XMLHttpRequest();
+        http.open("get", "product.json", true);
+        http.send();
+
+        http.onreadystatechange = function() {
+            if(http.readyState === 4) {
+                setProduct(JSON.parse(http.responseText));
+            }
+        }
+    }
+
+    useEffect(() => {
+        LoadProduct();
+    }, []);
 
     return(
         <div className="container-fluid">
-            <h2>Data Types</h2>
-            Stock : {(Stock == true) ? "Available" : "Out of Stock"}
+            <div className="mt-4 row">
+                <div className="col-3">
+                    <img src={product.image} width="100%" />
+                </div>
+                <div className="col-9">
+                    <div className="h3">
+                        {product.title}
+                    </div>
+                    <div>
+                        <span className="badge bg-success text-white p-2">{product.rating.rate} <span className=""></span></span>
+                        <span className="ms-3 text-secondary">{product.rating.ratings.toLocaleString()} Ratings & {product.rating.reviews} Reviews</span>
+                    </div>
+                    <div className="mt-3">
+                        <div className="h2 fw-bold">{product.price}</div>
+                    </div>
+                    <div className="mt-3">
+                        <div className="h5">Available</div>
+                        <ul className="list-unstyled">
+                            {
+                                product.offers.map(offer=>
+                                    <li key={offer} className="bi bi-tag-fill my-3">{offer}</li>
+                                )
+                            }
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
