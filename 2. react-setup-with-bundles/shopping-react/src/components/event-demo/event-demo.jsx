481cb42fd0cd4e484f08c15960740db27d260373
Shutirtha Roy <shutirtha.roy1@gmail.com>
2024-02-11 01:35:21 +1100

    disabling the default events configured for elements

--- 2. react-setup-with-bundles/shopping-react/src/components/event-demo/event-demo.jsx
+++ 2. react-setup-with-bundles/shopping-react/src/components/event-demo/event-demo.jsx
@@ -1,17 +1,15 @@
 export function EventDemo() {
 
-    function ContainerClick() {
-        alert("Container Clicked");
-    }
-
-    function ButtonClick(e) {
-        alert("Button Click");
-        //e.stopPropagation();
+    function SubmitClicked(e) {
+        alert('Later we will post data to API');
+        e.preventDefault();
     }
 
     return(
-        <div className="container-fluid m-2" onClick={ContainerClick}>
-            <button id="btnInsert" onClick={ButtonClick} className="btn btn-success mt-4">Insert</button>
+        <div className="container-fluid m-2 p-4">
+            <form class="w-50" onSubmit={SubmitClicked}>
+                User Name : <input type="text" name="Username" /><button type="submit">Submit</button>
+            </form>
         </div>
     )
 }
